902795668eac391a67de2ed8b4562c65fddde073
GustavoR77 <52572546+Zarddy77@users.noreply.github.com>
2019-08-28 01:22:39 -0300

    1.1
    1.1

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,28 +1,28 @@
 // Get data on firestore
 db.collection('assinantes').get().then(snapshot => {
-    setupGuides(snapshot.docs);
-});
+    setupGuides(snapshot.docs); 
+}); 
 
 // Listen for auth status changes
 auth.onAuthStateChanged(user => {
     if (user) {
         // Get data on firestore
         db.collection('assinantes').onSnapshot(snapshot => {
-        setupGuides(snapshot.docs);
-        setupUI(user);
+        setupGuides(snapshot.docs); 
+        setupUI(user); 
     }, err => {
         console.log(err.message)
-    });
+    }); 
     } else {
-        setupUI();
-        setupGuides([]);
+        setupUI(); 
+        setupGuides([]); 
     }
-});
+}); 
 
 // Create new guide
-const createForm = document.querySelector('#create-form');
+const createForm = document.querySelector('#create-form'); 
 createForm.addEventListener('submit', (e) => {
-    e.preventDefault();
+    e.preventDefault(); 
 
     db.collection('assinantes').add({
         empresa: createForm['title'].value,
@@ -30,57 +30,57 @@ createForm.addEventListener('submit', (e) => {
         assinatura: createForm['plano'].value
     }).then(() => {
         // Close the create modal and reset the form
-        const modal = document.querySelector('#modal-create');
-        M.Modal.getInstance(modal).close();
-        createForm.reset();
+        const modal = document.querySelector('#modal-create'); 
+        M.Modal.getInstance(modal).close(); 
+        createForm.reset(); 
     }).catch(err => {
         console.log(err.message)
-    });
-});
+    }); 
+}); 
 
 // Signup
-const signupForm = document.querySelector('#signup-form');
+const signupForm = document.querySelector('#signup-form'); 
 signupForm.addEventListener('submit', (e) => {
-    e.preventDefault();
+    e.preventDefault(); 
 
     // Get user info
-    const email = signupForm['signup-email'].value;
-    const password = signupForm['signup-password'].value;
+    const email = signupForm['signup-email'].value; 
+    const password = signupForm['signup-password'].value; 
 
     // Sign up the user
     auth.createUserWithEmailAndPassword(email, password).then(cred => {
         return db.collection('users').doc(cred.user.uid).set({
             nickname: signupForm['signup-nickname'].value
-        });
+        }); 
     }).then(() => {
         // Close the signup modal and reset the form
-        const modal = document.querySelector('#modal-signup');
-        M.Modal.getInstance(modal).close();
-        signupForm.reset();
-        });
-});
+        const modal = document.querySelector('#modal-signup'); 
+        M.Modal.getInstance(modal).close(); 
+        signupForm.reset(); 
+        }); 
+}); 
 
 // Logout
-const logout = document.querySelector('#logout');
+const logout = document.querySelector('#logout'); 
 logout.addEventListener('click', (e) => {
-    e.preventDefault();
+    e.preventDefault(); 
     auth.signOut()
-});
+}); 
 
 // Login
-const loginForm = document.querySelector('#login-form');
+const loginForm = document.querySelector('#login-form'); 
 loginForm.addEventListener('submit', (e) => {
-    e.preventDefault();
+    e.preventDefault(); 
 
     // Get user info
-    const email = loginForm['login-email'].value;
-    const password = loginForm['login-password'].value;
+    const email = loginForm['login-email'].value; 
+    const password = loginForm['login-password'].value; 
 
     // Sign in the user
     auth.signInWithEmailAndPassword(email, password).then(cred => {
         // Close the login modal and reset the form
-        const modal = document.querySelector('#modal-login');
-        M.Modal.getInstance(modal).close();
-        loginForm.reset();
-    });
-});
+        const modal = document.querySelector('#modal-login'); 
+        M.Modal.getInstance(modal).close(); 
+        loginForm.reset(); 
+    }); 
+}); 
