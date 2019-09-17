8e2cc2a8dafb4476f021cb0e5a3738e0eed0615f
aspache <52223516+aspache@users.noreply.github.com>
2019-09-17 19:30:14 -0300

    Att 4.0

--- Realtime Database/app.js
+++ Realtime Database/app.js
@@ -14,41 +14,29 @@
   firebase.initializeApp(firebaseConfig);
 
   // Get Elements
-  //const preObject = document.getElementById('object');
-  //const ulList = document.getElementById('list');
-  const nomeMes = document.getElementById('mensal');
+  const preObject = document.getElementById('object');
+  const ulList = document.getElementById('list');
 
-  // Get Database Reference
-  //const dbRefObject = firebase.database().ref().child('object');
-  //const dbRefList = dbRefObject.child('hobbies');
-  const dbRefMes = firebase.database().ref().child('mensal')
-
-  // Sync Mensal Changes
-  dbRefMes.on('value', snap => {
-    nomeMes.innerText = JSON.stringify(snap.val(),null,3);
-  });
+  // Create References
+  const dbRefObject = firebase.database().ref().child('financeiro');
+  const dbRefList = dbRefObject.child('mensal');
 
   // Sync Object Changes
-  //dbRefObject.on('value', snap => {
-  //    preObject.innerText = JSON.stringify(snap.val(), null, 3);
-  //});
+  dbRefObject.on('value', snap => {
+    preObject.innerText = JSON.stringify(snap.val(), null, 3);
+  });
 
-  //Sync List Changes
-  //dbRefList.on('child_added', snap => {
-  //    const li = document.createElement('li');
-  //    li.innerText = snap.val();
-  //    li.id = snap.key;
-  //    ulList.appendChild(li);
-  //});
+  // Sync List Changes
+  dbRefList.on('child_added', snap => {
+    const span = document.createElement('span');
+    span.innerText = snap.val();
+    span.id = snap.key;
+    ulList.appendChild(span);
+  });
 
-  //dbRefList.on('child_changed', snap => {
-  //    const liChanged = document.getElementById(snap.key);
-  //    liChanged.innerText = snap.val();
-  //});
-
-  //dbRefList.on('child_removed', snap => {
-  //  const liRemoved = document.getElementById(snap.key);
-  //  liRemoved.remove();
-  //});
+  dbRefList.on('child_changed', snap => {
+    const spanChanged = document.getElementById(snap.key);
+    spanChanged.innerText = snap.val();
+  });
 
 }());
